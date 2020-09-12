08998c71e6341945d478160addbd694fd57b2937
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-09-12 19:13:18 +0700

    Membuat Basic Component dengan hooks dan Slicing Halaman Register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { RegisterBg } from '../../assets'
+import { Button, Gap, Input, Link } from '../../components'
+import './register.scss'
 
 const Register = () => {
     return (
-        <div>
-            <p>Register Page</p>
+        <div className="main-page">
+            <div className="left">
+                <img src={RegisterBg} className="bg-image" alt="imageBg"></img>
+            </div>
+            <div className="right">
+                <p className="title">Register</p>
+                <Input label="Full Name" placeholder="Full Name"></Input>
+                <Gap height={18}/>
+                <Input label="Email" placeholder="Email"></Input>
+                <Gap height={18}/>
+                <Input label="Password" placeholder="Password"></Input>
+                <Gap height={50}/>
+                <Button title="Register"></Button>
+                <Gap height={100}/>
+                <Link title="Kembali ke Login"></Link>
+            </div>
         </div>
     )
 }
 
-export default Register
+export default Register;
