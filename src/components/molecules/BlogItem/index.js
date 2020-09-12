c4a979cd3ae49716c2c187e99f57f4b771013c86
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-09-12 22:48:57 +0700

    Membuat Styling Item Blog Post dan Pagination

---
+++ src/components/molecules/BlogItem/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { RegisterBg } from '../../../assets'
+import './blogItem.scss'
+
+const BlogItem = () => {
+    return (
+        <div className="blog-item">
+            <img className="image-thumb" src={RegisterBg} alt="post"></img>
+            <div className="content-detail">
+                <p className="title">Title blog</p>
+                <p className="author">Author - Date post</p>
+                <p className="body">Eiusmod culpa veniam magna Lorem pariatur exercitation eiusmod proident ad Lorem id voluptate. Quis elit nostrud nisi elit consequat ullamco nostrud eu culpa commodo sunt elit ad fugiat. In culpa veniam ex proident duis irure amet cupidatat duis quis ex pariatur in. Proident dolore nostrud et sint sint adipisicing. Duis tempor veniam irure culpa magna excepteur exercitation laborum amet nisi nostrud consectetur laborum. Sint nostrud dolore ad sit.</p>
+            </div>
+        </div>
+    )
+}
+
+export default BlogItem
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BlogItem, Button, Gap } from '../../components'
+import './home.scss'
+
 
 const Home = () => {
     return (
-        <div>
-            <p>Home Page</p>
+        <div className="home-page-wrapper">
+            <div className="create-wrapper">
+                <Button title="Create Blog"></Button>
+            </div>
+            <Gap height={20}></Gap>
+            <div className="content-wrapper">
+                <BlogItem></BlogItem>
+                <BlogItem></BlogItem>
+                <BlogItem></BlogItem>
+                <BlogItem></BlogItem>
+            </div>
+            <div className="pagination">
+                <Button title="Previous"></Button>
+                <Gap width={20}></Gap>
+                <Button title="Next"></Button>
+            </div>
+                <Gap height={20}></Gap>
         </div>
     )
 }
